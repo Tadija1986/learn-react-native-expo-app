96aff3cea53a09534981211cdde3918a1f538a5b
Tadija1986 <tadija14@gmail.com>
2021-03-24 12:23:45 +0100

    review app created and installed expo-add-loading

--- App.js
+++ App.js
@@ -3,80 +3,117 @@ import React, { useState } from "react";
 import {
   StyleSheet,
   View,
+  Text,
   FlatList,
   Alert,
   TouchableWithoutFeedback,
   Keyboard,
 } from "react-native";
-import Header from "./components/header";
-import ToDoItem from "./components/todoItem";
-import AddToDoItem from "./components/addToDoItem";
-import Sandbox from "./components/sandbox";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { useFonts } from "expo-font";
+import Home from "./screens/home.js";
+import About from "./screens/about.js";
+import ReviewDetails from "./screens/reviewDetails.js";
 
 export default function App() {
-  const [toDoList, setToDoList] = useState([
-    { text: "buy coffee", key: "1" },
-    { text: "create app", key: "2" },
-    { text: "play on the switch", key: "3" },
-  ]);
-  const pressHandler = (key) => {
-    setToDoList((prevToDoList) => {
-      return prevToDoList.filter((toDoItem) => toDoItem.key !== key);
-    });
-  };
-  const submitHandler = (text) => {
-    if (text.length > 3) {
-      setToDoList((prevToDoList) => {
-        return [{ text: text, key: Math.random().toString() }, ...prevToDoList];
-      });
-    } else {
-      Alert.alert("Alert", "To do item must be minimum 3 chars.", [
-        {
-          text: "Ok",
-          onPress: () => console.log("Alert closed"),
-        },
-      ]);
-    }
-  };
-  return (
-    // <Sandbox />
-    <TouchableWithoutFeedback
-      onPress={() => {
-        Keyboard.dismiss();
-      }}
-    >
-      <View style={styles.container}>
-        <Header />
-        <View style={styles.content}>
-          <AddToDoItem submitHandler={submitHandler} />
-          <View style={styles.list}>
-            <FlatList
-              data={toDoList}
-              renderItem={({ item }) => (
-                <ToDoItem item={item} pressHandler={pressHandler} />
-              )}
-            />
-          </View>
-        </View>
-        <StatusBar style="auto" />
-      </View>
-    </TouchableWithoutFeedback>
-  );
+  let [fontsLoaded] = useFonts({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return <Home />;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  content: {
-    flex: 1,
-    padding: 40,
-    backgroundColor: "yellow",
-  },
-  list: {
-    flex: 1,
-    marginTop: 40,
-    backgroundColor: "lightgreen",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//   },
+//   content: {
+//     flex: 1,
+//     padding: 40,
+//     backgroundColor: "yellow",
+//   },
+//   list: {
+//     flex: 1,
+//     marginTop: 40,
+//     backgroundColor: "lightgreen",
+//   },
+// });
+
+// import Header from "./components/header";
+// import ToDoItem from "./components/todoItem";
+// import AddToDoItem from "./components/addToDoItem";
+// import Sandbox from "./components/sandbox";
+
+// export default function App() {
+//   const [toDoList, setToDoList] = useState([
+//     { text: "buy coffee", key: "1" },
+//     { text: "create app", key: "2" },
+//     { text: "play on the switch", key: "3" },
+//   ]);
+//   const pressHandler = (key) => {
+//     setToDoList((prevToDoList) => {
+//       return prevToDoList.filter((toDoItem) => toDoItem.key !== key);
+//     });
+//   };
+//   const submitHandler = (text) => {
+//     if (text.length > 3) {
+//       setToDoList((prevToDoList) => {
+//         return [{ text: text, key: Math.random().toString() }, ...prevToDoList];
+//       });
+//     } else {
+//       Alert.alert("Alert", "To do item must be minimum 3 chars.", [
+//         {
+//           text: "Ok",
+//           onPress: () => console.log("Alert closed"),
+//         },
+//       ]);
+//     }
+//   };
+//   return (
+//     // <Sandbox />
+//     <TouchableWithoutFeedback
+//       onPress={() => {
+//         Keyboard.dismiss();
+//       }}
+//     >
+//       <View style={styles.container}>
+//         <Header />
+//         <View style={styles.content}>
+//           <AddToDoItem submitHandler={submitHandler} />
+//           <View style={styles.list}>
+//             <FlatList
+//               data={toDoList}
+//               renderItem={({ item }) => (
+//                 <ToDoItem item={item} pressHandler={pressHandler} />
+//               )}
+//             />
+//           </View>
+//         </View>
+//         <StatusBar style="auto" />
+//       </View>
+//     </TouchableWithoutFeedback>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//   },
+//   content: {
+//     flex: 1,
+//     padding: 40,
+//     backgroundColor: "yellow",
+//   },
+//   list: {
+//     flex: 1,
+//     marginTop: 40,
+//     backgroundColor: "lightgreen",
+//   },
+// });
--- components/todoItem.js
+++ components/todoItem.js
@@ -1,21 +1,30 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function ToDoItem({ item, pressHandler }) {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
+      <View style={styles.item}>
+        <Text style={styles.itemText}>{item.text}</Text>
+        <MaterialIcons name="delete" size={18} color="#333" />
+      </View>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   item: {
+    flex: 1,
+    flexDirection: "row",
     padding: 16,
     marginTop: 16,
     borderColor: "#bbb",
     borderWidth: 1,
     borderStyle: "dashed",
     borderRadius: 10,
+  },
+  itemText: {
+    marginLeft: 10,
   },
 });
